fdc147fb8858dc1171e0357e64dd13fa88452dab
ilyabaykov <i-baykov@internet.ru>
2024-02-13 13:48:03 +0300

    Создал модель слов

--- engLearn/models.py
+++ engLearn/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Words(models.Model):
+    en_word = models.CharField(max_length=45)
+    ru_word = models.CharField(max_length=45)
+    en_example = models.TextField()
+    ru_example = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f'{self.en_word} - {self.ru_word}'
