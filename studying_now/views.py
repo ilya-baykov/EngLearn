51d4cdc124e82fd9cc64493c86d11d247706ab1c
ilyabaykov <i-baykov@internet.ru>
2024-02-21 23:34:39 +0300

    Теперь отображается карточка каждого сохраненного слова

--- studying_now/views.py
+++ studying_now/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView
 from . import models
+from itertools import chain
 
 
 # Create your models here.
@@ -14,4 +15,6 @@ class StudyingNowListView(ListView):
     context_object_name = 'save_words'
 
     def get_queryset(self):
-        return models.StudyingNowModel.objects.filter(user=self.request.user)
+        studying_now_objects = models.StudyingNowModel.objects.filter(user=self.request.user)
+        studying_words = chain.from_iterable([user.studying_now_word.all() for user in studying_now_objects])
+        return studying_words
