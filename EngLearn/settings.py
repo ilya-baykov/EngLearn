6a998104ffdd11292e01e2d12f31ee3f6ee2b418
ilyabaykov <i-baykov@internet.ru>
2024-02-21 22:45:31 +0300

    Создал модель StudyingNowModel, которая связывает изучаемые слова с конкретным пользователем

--- EngLearn/settings.py
+++ EngLearn/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'engLearn.apps.EnglearnConfig',
+    'studying_now.apps.StudyingNowConfig'
 ]
 
 MIDDLEWARE = [
