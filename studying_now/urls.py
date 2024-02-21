082a310954420c01d264bb69edc7b03174474fae
ilyabaykov <i-baykov@internet.ru>
2024-02-21 23:40:19 +0300

    Теперь сохраненные картинки показываются только тем пользователям, которые авторизовались на сайте /  иначе сайт предложит зайти под своей учетной записью

--- studying_now/urls.py
+++ studying_now/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
-    path('', views.StudyingNowListView.as_view(), name='studying_now'),
+    path('', login_required(views.StudyingNowListView.as_view()), name='studying_now'),
 ]
