91cebd9fb0074ffe208d478dd60b6170a2296cfe
ilyabaykov <i-baykov@internet.ru>
2024-03-05 01:41:01 +0300

    Починил БазуДанных

--- EngLearn/settings.py
+++ EngLearn/settings.py
@@ -75,14 +75,18 @@ WSGI_APPLICATION = 'EngLearn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'englearn',
         'USER': 'englearnadmin',
         'PASSWORD': 'adminpassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
