d7e2936112d0ea9b7b522ee25d51fa750ab62c29
ilyabaykov <i-baykov@internet.ru>
2024-03-05 20:01:50 +0300

    Теперь при успешном сохранении слова - появляется сообщение об этом

--- engLearn/views.py
+++ engLearn/views.py
@@ -2,14 +2,18 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.contrib.auth.models import User
 from studying_now.models import StudyingNowModel
 from .models import Words
-from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 
 app_name = 'engLearn'
+
+
+@login_required
+def profile(request):
+    return render(request, 'engLearn/profile.html')
 
 
 class WordsListView(ListView):
@@ -30,27 +34,26 @@ class WordsDetailView(DetailView):
     slug_url_kwarg = 'word_slug'
 
     def get_context_data(self, **kwargs):
-        contex = super().get_context_data(**kwargs)
-        contex['title'] = 'Word Details'
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Word Details'
         word = self.get_object()
         studying_now, created = StudyingNowModel.objects.get_or_create(user=self.request.user)
         in_list = word in studying_now.studying_now_word.all()
-        contex['in_list'] = in_list
-        return contex
-
-
-@login_required
-def profile(request):
-    return render(request, 'engLearn/profile.html')
+        context['in_list'] = in_list
+        return context
 
 
 def add_to_studying_now(request, word_slug):
     if request.method == 'POST':
         word = Words.objects.get(slug=word_slug)
-        studying_now, created = StudyingNowModel.objects.get_or_create(user=request.user)
+        studying_now = StudyingNowModel.objects.get(user=request.user)
+        print(studying_now)
         if word in studying_now.studying_now_word.all():
             messages.warning(request, f'Слово "{word}" уже сохранено для изучения.')
+            in_list = True
         else:
             studying_now.studying_now_word.add(word)
             messages.success(request, f'Слово "{word}" успешно добавлено в список изучения.')
-        return HttpResponseRedirect(reverse('word_detail', args=(word_slug,)))
+            in_list = False
+        uri = reverse('word_detail', args=(word_slug,))
+        return HttpResponseRedirect(uri, messages)
