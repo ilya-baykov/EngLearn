80150d0b8f487f220ce6289dd7132c95bda1b1f7
ilyabaykov <i-baykov@internet.ru>
2024-03-05 14:17:56 +0300

    Теперь слова для изучения отображаются в правильном порядке

--- studying_now/models.py
+++ studying_now/models.py
@@ -6,8 +6,11 @@ from engLearn.models import Words
 
 class StudyingNowModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    studying_now_word = models.ManyToManyField(Words, related_name='studying_now_word')
+    studying_now_word = models.ManyToManyField(Words, related_name='studying_now_word', blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user.username} - {len(self.studying_now_word.all())}'
+
+    class Meta:
+        ordering = ('-date_added',)
--- studying_now/views.py
+++ studying_now/views.py
@@ -2,8 +2,13 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 # Create your views here.
 from django.views.generic import ListView
+
+from engLearn.models import Words
 from . import models
 from itertools import chain
+from operator import attrgetter
+
+from .models import StudyingNowModel
 
 
 # Create your models here.
@@ -15,8 +20,8 @@ class StudyingNowListView(ListView):
     context_object_name = 'save_words'
 
     def get_queryset(self):
-        studying_now_objects = models.StudyingNowModel.objects.filter(user=self.request.user)
-        studying_words = chain.from_iterable([user.studying_now_word.all() for user in studying_now_objects])
+        studying_now_objects = StudyingNowModel.objects.filter(user=self.request.user)
+        studying_words = Words.objects.filter(studying_now_word__in=studying_now_objects).order_by(
+            '-studying_now_word__date_added')
+
         return studying_words
-
-
