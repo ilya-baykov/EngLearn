914c57df07b1ff00e1cafd2921c439dd5c4afae8
ilyabaykov <i-baykov@internet.ru>
2024-03-07 20:50:10 +0300

    Теперь у каждого пользователя сохраняются только свои карточки

--- EngLearn/settings.py
+++ EngLearn/settings.py
@@ -77,20 +77,20 @@ WSGI_APPLICATION = 'EngLearn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'englearn',
-        'USER': 'englearnadmin',
-        'PASSWORD': 'adminpassword',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'englearn',
+#         'USER': 'englearnadmin',
+#         'PASSWORD': 'adminpassword',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
