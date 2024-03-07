130dd1d2e08343a9c984e2fcad5457b434e6c2b5
ilyabaykov <i-baykov@internet.ru>
2024-03-07 18:57:33 +0300

    Теперь у пользователя есть возможность сохранять свои слова на сайт. Картинки также можно добавлять*

--- my_added_words/urls.py
+++ my_added_words/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.MyWordsListView.as_view(), name='my_added_words_list'),
+    path('add_word/', views.add_to_my_added_word, name='add_to_my_added_word'),
     path('remove_word/<slug:word_slug>/', views.remove_from_my_added_word, name='remove_from_my_added_word'),
 
 ]
