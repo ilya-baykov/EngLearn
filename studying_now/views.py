71406ac1711944f340411f7aa7c45f629f616dec
ilyabaykov <i-baykov@internet.ru>
2024-03-16 03:25:20 +0300

    Восстановил работу всех функций через использования новой модели

--- studying_now/views.py
+++ studying_now/views.py
@@ -6,10 +6,6 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
 
-
-# Create your models here.
-
-
 class StudyingNowListView(ListView):
     model = models.StudyingNowModel
     template_name = 'studying_now/studying_now_list.html'
@@ -18,7 +14,7 @@ class StudyingNowListView(ListView):
     def get_queryset(self):
         studying_now_objects = StudyingNowModel.objects.filter(user=self.request.user)
         studying_now_sorted = studying_now_objects.order_by('-date_added')
-        studying_words = Words.objects.filter(studying_now_word__in=studying_now_sorted)
+        studying_words = Words.objects.filter(studyingnowmodel__in=studying_now_sorted)
         return studying_words
 
     def get_context_data(self, **kwargs):
@@ -35,7 +31,6 @@ def add_to_studying_now(request, word_slug):
         if user_studying_now_words.filter(word=word):
             messages.warning(request, f'Слово "{word}" уже сохранено для изучения.')
         else:
-            # request.user.in_study_words.add(word)
             StudyingNowModel.objects.get_or_create(user=request.user, word=word)
             messages.success(request, f'Слово "{word}" успешно добавлено в список изучения.')
 
@@ -47,8 +42,7 @@ def add_to_studying_now(request, word_slug):
 def remove_from_studying_now(request, word_slug):
     if request.method == 'POST':
         word = Words.objects.get(slug=word_slug)
-        request.user.in_study_words.remove(word)
-        # studying_now_model = StudyingNowModel.objects.get(user=request.user, word=word)
-        # studying_now_model.delete()
+        studying_now_model = StudyingNowModel.objects.get(user=request.user, word=word)
+        studying_now_model.delete()
         uri = reverse('studying_now')
         return HttpResponseRedirect(uri)
