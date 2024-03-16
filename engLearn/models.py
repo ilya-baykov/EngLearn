6c017d8c2a8662d545f33dd9312c68e8a35a6228
ilyabaykov <i-baykov@internet.ru>
2024-03-17 00:17:48 +0300

    Создал модель , через которую планирую дать пользователям возможность добавлять свои примеры

--- engLearn/models.py
+++ engLearn/models.py
@@ -11,6 +11,8 @@ class Words(models.Model):
     img_link = models.URLField(blank=True)
     in_study = models.ManyToManyField(User, related_name='in_study_words', through="studying_now.StudyingNowModel",
                                       through_fields=('word', 'user'))
+    user_example = models.ManyToManyField(User, related_name='user_example', through="engLearn.WordExamples",
+                                          through_fields=('word', 'user'))
 
     picurl = models.TextField(blank=True)
     picau = models.CharField(max_length=60)
@@ -21,3 +23,14 @@ class Words(models.Model):
 
     def __str__(self):
         return f'{self.en_word} - {self.ru_word}'
+
+
+class WordExamples(models.Model):
+    word = models.ForeignKey(Words, on_delete=models.CASCADE, related_name='word')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    en_example_user = models.TextField()
+    ru_example_user = models.TextField()
+
+    def __str__(self):
+        return f'{self.user} - {self.en_example_user}'
+from django.contrib.auth.models import User
