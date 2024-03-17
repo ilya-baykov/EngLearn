ebd79309a8025ce94efc70f8ad6a0403662bbfa8
ilyabaykov <i-baykov@internet.ru>
2024-03-17 23:28:42 +0300

    Реализовал возможность добавления своих примеров в "мною добавленные слова"

--- my_added_words/urls.py
+++ my_added_words/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', login_required(views.MyWordsListView.as_view()), name='my_added_words_list'),
     path('add_word/', views.add_to_my_added_word, name='add_to_my_added_word'),
     path('remove_word/<slug:word_slug>/', views.remove_from_my_added_word, name='remove_from_my_added_word'),
+    path('add_examples/<slug:word_slug>/', views.add_examples_my_added_word, name='add_examples_my_added_word'),
 
 ]
